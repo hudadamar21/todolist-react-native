152de316c41a31ddc819a6b5ceea54382db60bbb
hudadamar21 <hudadamar21@gmail.com>
2023-06-15 21:50:27 +0700

    refactor home page todo list hoek

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -1,22 +1,23 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useMemo} from 'react';
 
-import {Text, TextInput, View, TouchableOpacity, FlatList} from 'react-native';
+import {Text, View, FlatList} from 'react-native';
 import TodoItem from '../components/TodoItem';
 import {Todo} from '../types/Todo';
 import {useAtom} from 'jotai';
 import {todosAtom} from '../store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import TodoInputField from '../components/TodoInputField';
 
 const HomePage = () => {
   const [todos, setTodos] = useAtom(todosAtom);
 
-  const [isEdit, setIsEdit] = useState<boolean>(false);
-  const [editId, setEditId] = useState<string>();
-  const [inputValue, setInputValue] = useState<string>('');
-
-  const todoEdit = useMemo<Todo>(
-    () => todos.filter(todo => todo.id === editId)[0],
-    [editId, todos],
+  const todoNotComplete = useMemo(
+    () => todos.filter(todo => !todo.isComplete),
+    [todos],
+  );
+  const todoIsComplete = useMemo(
+    () => todos.filter(todo => todo.isComplete),
+    [todos],
   );
 
   useEffect(() => {
@@ -34,90 +35,14 @@ const HomePage = () => {
     loadData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const handleEdit = (id: string) => {
-    setEditId(id);
-    setIsEdit(true);
-    setInputValue(todoEdit?.name);
-  };
-
-  const clearState = () => {
-    setEditId('');
-    setIsEdit(false);
-    setInputValue('');
-  };
-
-  const handleCreate = () => {
-    if (inputValue) {
-      setTodos(currentTodos => [
-        ...currentTodos,
-        {
-          id: (currentTodos.length + 1).toString(),
-          name: inputValue,
-          date: new Date().toLocaleString(),
-          isComplete: false,
-        },
-      ]);
-      clearState();
-    }
-  };
-  const handleUpdate = () => {
-    setTodos(currentTodos =>
-      currentTodos.map(todo =>
-        todo.id === editId ? {...todo, name: inputValue} : todo,
-      ),
-    );
-    clearState();
-  };
-  const handleComplete = (id: string) => {
-    setTodos(currentTodos =>
-      currentTodos.map(todo =>
-        todo.id === id ? {...todo, isComplete: !todo.isComplete} : todo,
-      ),
-    );
-    clearState();
-  };
-  const handleDelete = (id: string) => {
-    setTodos(currTodos => currTodos.filter(todo => todo.id !== id));
-    console.log(todos);
-  };
-
   return (
     <View className="p-3">
       <Text className="text-2xl font-bold mt-3 mb-5 text-black">Todo List</Text>
-      <View className="flex flex-row w-full mb-5">
-        <TextInput
-          className="flex-grow h-12 text-base border border-gray-200 rounded-lg px-5"
-          placeholder="your todo.."
-          onChangeText={setInputValue}
-          value={inputValue}
-        />
-        {!isEdit ? (
-          <TouchableOpacity
-            activeOpacity={0.9}
-            className=" bg-blue-500 flex justify-center items-center ml-2 text-white w-20 rounded-lg"
-            onPress={handleCreate}>
-            <Text className="text-white flex items-center">Create</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            className="bg-blue-500 flex justify-center items-center ml-2 text-white w-20 rounded-lg"
-            onPress={handleUpdate}>
-            <Text className="text-white flex items-center">Update</Text>
-          </TouchableOpacity>
-        )}
-      </View>
+      <TodoInputField />
       <View>
         <FlatList
-          data={todos.filter(todo => !todo.isComplete)}
-          renderItem={({item}: {item: Todo}) =>
-            TodoItem({
-              item,
-              onEdit: handleEdit,
-              onDelete: handleDelete,
-              onComplete: handleComplete,
-            })
-          }
+          data={todoNotComplete}
+          renderItem={({item}: {item: Todo}) => <TodoItem item={item} />}
           keyExtractor={(item: Todo) => item.id}
         />
       </View>
@@ -126,15 +51,8 @@ const HomePage = () => {
           Todo Completed:
         </Text>
         <FlatList
-          data={todos.filter(todo => todo.isComplete)}
-          renderItem={({item}: {item: Todo}) =>
-            TodoItem({
-              item,
-              onEdit: handleEdit,
-              onDelete: handleDelete,
-              onComplete: handleComplete,
-            })
-          }
+          data={todoIsComplete}
+          renderItem={({item}: {item: Todo}) => <TodoItem item={item} />}
           keyExtractor={(item: Todo) => item.id}
         />
       </View>
